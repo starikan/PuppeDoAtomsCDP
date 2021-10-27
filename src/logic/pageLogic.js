55a917fa4ea7dd96fc39dab3db07b61d26a6c481
sstarodubov <s.starodubov@litebox.ru>
2021-10-27 18:42:47 +0400

    Split logic on files

--- src/logic/pageLogic.js
+++ src/logic/pageLogic.js
@@ -120,4 +120,90 @@ const switchLoader = (flag = true) => {
   loader.style.setProperty('display', flag ? 'grid' : 'none');
 };
 
-module.exports = { runDialog, jsEvalOnClick, sendDataToDialog, switchLoader };
+const dialogDrawer = (dialogId) => {
+  window.dialogDrawer = (data) => {
+    const content = document.querySelector(`#${dialogId} .content`);
+    content.innerHTML = '<pre>' + JSON.stringify(data, null, 2) + '</pre>';
+  };
+};
+
+const addDialogHTML = (dialogId) => {
+  const body = document.getElementsByTagName('body');
+  const div = document.createElement('div');
+  div.setAttribute('id', dialogId);
+  div.setAttribute('class', 'dialog');
+  div.innerHTML = `
+  <div class="titlebar">PPD Creator</div>
+  <button name="collapse"></button>
+  <button name="fullscreen"></button>
+    <div id="content" class="content"></div>
+    <div class="buttonpane">
+      <div class="buttonset">
+        <button name="ok">OK</button>
+        <button name="cancel">Cancel</button>
+      </div>
+    </div>
+  `;
+  body[0].appendChild(div);
+  const waiter = document.createElement('div');
+  waiter.setAttribute('id', 'ppd-wait-data-process-wraper');
+  waiter.innerHTML = `
+    <div id="ppd-wait-data-process">
+      <?xml version="1.0" encoding="utf-8"?>
+      <svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" style="margin: auto; background: rgb(241, 242, 243); display: block; shape-rendering: auto;" width="200px" height="200px" viewBox="0 0 100 100" preserveAspectRatio="xMidYMid">
+      <g transform="rotate(0 50 50)">
+        <rect x="47" y="24" rx="3" ry="6" width="6" height="12" fill="#1d3f72">
+          <animate attributeName="opacity" values="1;0" keyTimes="0;1" dur="1s" begin="-0.9166666666666666s" repeatCount="indefinite"></animate>
+        </rect>
+      </g><g transform="rotate(30 50 50)">
+        <rect x="47" y="24" rx="3" ry="6" width="6" height="12" fill="#1d3f72">
+          <animate attributeName="opacity" values="1;0" keyTimes="0;1" dur="1s" begin="-0.8333333333333334s" repeatCount="indefinite"></animate>
+        </rect>
+      </g><g transform="rotate(60 50 50)">
+        <rect x="47" y="24" rx="3" ry="6" width="6" height="12" fill="#1d3f72">
+          <animate attributeName="opacity" values="1;0" keyTimes="0;1" dur="1s" begin="-0.75s" repeatCount="indefinite"></animate>
+        </rect>
+      </g><g transform="rotate(90 50 50)">
+        <rect x="47" y="24" rx="3" ry="6" width="6" height="12" fill="#1d3f72">
+          <animate attributeName="opacity" values="1;0" keyTimes="0;1" dur="1s" begin="-0.6666666666666666s" repeatCount="indefinite"></animate>
+        </rect>
+      </g><g transform="rotate(120 50 50)">
+        <rect x="47" y="24" rx="3" ry="6" width="6" height="12" fill="#1d3f72">
+          <animate attributeName="opacity" values="1;0" keyTimes="0;1" dur="1s" begin="-0.5833333333333334s" repeatCount="indefinite"></animate>
+        </rect>
+      </g><g transform="rotate(150 50 50)">
+        <rect x="47" y="24" rx="3" ry="6" width="6" height="12" fill="#1d3f72">
+          <animate attributeName="opacity" values="1;0" keyTimes="0;1" dur="1s" begin="-0.5s" repeatCount="indefinite"></animate>
+        </rect>
+      </g><g transform="rotate(180 50 50)">
+        <rect x="47" y="24" rx="3" ry="6" width="6" height="12" fill="#1d3f72">
+          <animate attributeName="opacity" values="1;0" keyTimes="0;1" dur="1s" begin="-0.4166666666666667s" repeatCount="indefinite"></animate>
+        </rect>
+      </g><g transform="rotate(210 50 50)">
+        <rect x="47" y="24" rx="3" ry="6" width="6" height="12" fill="#1d3f72">
+          <animate attributeName="opacity" values="1;0" keyTimes="0;1" dur="1s" begin="-0.3333333333333333s" repeatCount="indefinite"></animate>
+        </rect>
+      </g><g transform="rotate(240 50 50)">
+        <rect x="47" y="24" rx="3" ry="6" width="6" height="12" fill="#1d3f72">
+          <animate attributeName="opacity" values="1;0" keyTimes="0;1" dur="1s" begin="-0.25s" repeatCount="indefinite"></animate>
+        </rect>
+      </g><g transform="rotate(270 50 50)">
+        <rect x="47" y="24" rx="3" ry="6" width="6" height="12" fill="#1d3f72">
+          <animate attributeName="opacity" values="1;0" keyTimes="0;1" dur="1s" begin="-0.16666666666666666s" repeatCount="indefinite"></animate>
+        </rect>
+      </g><g transform="rotate(300 50 50)">
+        <rect x="47" y="24" rx="3" ry="6" width="6" height="12" fill="#1d3f72">
+          <animate attributeName="opacity" values="1;0" keyTimes="0;1" dur="1s" begin="-0.08333333333333333s" repeatCount="indefinite"></animate>
+        </rect>
+      </g><g transform="rotate(330 50 50)">
+        <rect x="47" y="24" rx="3" ry="6" width="6" height="12" fill="#1d3f72">
+          <animate attributeName="opacity" values="1;0" keyTimes="0;1" dur="1s" begin="0s" repeatCount="indefinite"></animate>
+        </rect>
+      </g>
+      </svg>
+    </div>
+  `;
+  body[0].appendChild(waiter);
+};
+
+module.exports = { runDialog, jsEvalOnClick, sendDataToDialog, switchLoader, dialogDrawer, addDialogHTML };
