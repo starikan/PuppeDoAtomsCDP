eae461d916b7de3cec9f6ea2b6045bb9ec5c2b29
sstarodubov <s.starodubov@litebox.ru>
2021-10-27 12:55:17 +0400

    Split logic on files

---
+++ src/logic/cdpLogic.ts
@@ -0,0 +1,73 @@
+const getCombinations = (charsInput: string[], divider = '.'): string[] => {
+  const result = [];
+  const f = (prefix: string, chars: string[]): void => {
+    for (let i = 0; i < chars.length; i += 1) {
+      result.push(prefix + divider + chars[i]);
+      f(prefix + divider + chars[i], chars.slice(i + 1));
+    }
+  };
+  f('', charsInput);
+  return result;
+};
+
+export const checkSelectorsInDom = (selectors: string[] = []): Array<[string, number]> => {
+  const selectorsStat = selectors.map((v) => [v, document.querySelectorAll(v).length] as [string, number]);
+  return selectorsStat;
+};
+
+export const generateSelectors = (elements: HTMLElement[]): string[] => {
+  const preparedElements = elements
+    .filter((v) => !v.hidden)
+    .filter((v) => v.tagName && !['HTML', 'BODY'].includes(v.tagName))
+    .map((v) => ({
+      classList: Array.from(v.classList),
+      attributes: v.attributes,
+      id: v.id,
+      tagName: v.tagName.toLowerCase(),
+      textContent: v.textContent,
+    }));
+  // .map((v: Partial<HTMLElement>) => {
+  //   v.tagName = v.tagName.toLowerCase();
+  //   return v;
+  // })
+  // .map((v: Partial<HTMLElement>) => {
+  //   delete v.attributes?.class;
+  //   return v;
+  // });
+
+  const parts = preparedElements.map((v) => {
+    const set = [
+      ...(v.id ? [`#${v.id}`] : []),
+      ...getCombinations(v.classList)
+        .map((c) => `${c}`)
+        .sort((a, b) => a.length - b.length),
+      // ...getCombinations(v.classList)
+      //   .map(c => `${v.tagName}${c}`)
+      //   .sort((a, b) => a.length - b.length),
+      ...getCombinations(
+        Object.entries(v.attributes).map((attr) => `[${attr[0]}='${attr[1]}']`),
+        '',
+      ),
+      // ...getCombinations(
+      //   Object.entries(v.attributes).map(attr => `[${attr[0]}='${attr[1]}']`),
+      //   '',
+      // ).map(c => `${v.tagName}${c}`),
+      ...[v.tagName],
+    ];
+
+    return set;
+  });
+
+  let selectors = [...parts[0]];
+  let combines = [...parts[0]];
+  for (let i = 1; i < parts.length; i += 1) {
+    // eslint-disable-next-line no-loop-func
+    combines = parts[i].reduce((s, b) => {
+      const summary = [...s, ...combines.map((a) => `${b} > ${a}`)];
+      return summary;
+    }, []);
+    selectors = [...selectors, ...combines];
+  }
+
+  return selectors;
+};
---
+++ src/logic/pageLogic.js
@@ -0,0 +1,114 @@
+const jsEvalOnClick = () => {
+  let ppdPreviousBorder = null;
+  let ppdElementClicked = null;
+
+  window.ppdClickHandler = function (event) {
+    if (!event.ctrlKey) {
+      return true;
+    }
+    event.stopPropagation();
+    event.preventDefault();
+
+    if (ppdElementClicked) {
+      ppdElementClicked.style.border = ppdPreviousBorder;
+    }
+
+    ppdElementClicked = event.target;
+    ppdElementClicked.style.setProperty('border', ppdPreviousBorder);
+
+    const exportData = {
+      x: event.x,
+      y: event.y,
+      button: event.button,
+      clientX: event.clientX,
+      clientY: event.clientY,
+      ctrlKey: event.ctrlKey,
+      layerX: event.layerX,
+      layerY: event.layerY,
+      metaKey: event.metaKey,
+      movementX: event.movementX,
+      movementY: event.movementY,
+      offsetX: event.offsetX,
+      offsetY: event.offsetY,
+      pageX: event.pageX,
+      pageY: event.pageY,
+      screenX: event.screenX,
+      screenY: event.screenY,
+      shiftKey: event.shiftKey,
+    };
+
+    exportData.path = [];
+    event.path.forEach((p, i) => {
+      if (i > 3) return;
+
+      const path = {
+        baseURI: p.baseURI,
+        childElementCount: p.childElementCount,
+        className: p.className,
+        clientHeight: p.clientHeight,
+        clientLeft: p.clientLeft,
+        clientTop: p.clientTop,
+        clientWidth: p.clientWidth,
+        draggable: p.draggable,
+        hidden: p.hidden,
+        id: p.id,
+        localName: p.localName,
+        nodeName: p.nodeName,
+        nodeType: p.nodeType,
+        nodeValue: p.nodeValue,
+        offsetHeight: p.offsetHeight,
+        offsetLeft: p.offsetLeft,
+        offsetTop: p.offsetTop,
+        offsetWidth: p.offsetWidth,
+        scrollHeight: p.scrollHeight,
+        scrollLeft: p.scrollLeft,
+        scrollTop: p.scrollTop,
+        scrollWidth: p.scrollWidth,
+        tabIndex: p.tabIndex,
+        tagName: p.tagName,
+        textContent: p.textContent,
+        title: p.title,
+      };
+
+      if (i === 0) {
+        path.innerHTML = p.innerHTML;
+        path.innerText = p.innerText;
+        path.outerHTML = p.outerHTML;
+        path.outerText = p.outerText;
+        path.text = p.text;
+      }
+
+      path.attributes = {};
+      if (p.attributes && p.attributes.length) {
+        for (let attr of p.attributes) {
+          path.attributes[attr.name] = attr.value;
+        }
+      }
+
+      path.classList = p.classList && p.classList.length ? [...p.classList] : [];
+
+      exportData.path.push(path);
+    });
+
+    const target = event.target;
+    exportData.xpath = [xpath.getXPath(target), xpath.getUniqueXPath(target)];
+    exportData.type = 'selectorClick';
+
+    // console.log(exportData);
+    console.log(JSON.stringify(exportData, { skipInvalid: true }));
+
+    target.style.setProperty('border', '2px solid red');
+  };
+  window.removeEventListener('click', window.ppdClickHandler);
+  window.addEventListener('click', window.ppdClickHandler, true);
+};
+
+const runDialog = (dialogId) => {
+  const callbackFunction = (data) => {
+    console.log(JSON.stringify({ button: data, type: 'servise' }));
+  };
+  const dialog = DialogBox(dialogId, callbackFunction);
+  dialog.showDialog();
+};
+
+module.exports = { runDialog, jsEvalOnClick };
